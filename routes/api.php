b5f40416b3a026e571b7666f01368439819725ec
saktialfansyahp <saktialfansyahp@gmail.com>
2023-07-29 13:37:59 +0700

    refactor(api): tambah endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Models\Kendaraan;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\KelasController;
 use App\Http\Controllers\MobilController;
 use App\Http\Controllers\MotorController;
@@ -23,13 +25,28 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('createMobil', [MobilController::class, 'create']);
-Route::delete('deleteMobil/{id}', [MobilController::class, 'delete']);
-Route::get('getMobil', [MobilController::class, 'get']);
+Route::post('login', [AuthController::class, 'login']);
+Route::post('register', [AuthController::class, 'register']);
 
-Route::post('createMotor', [MotorController::class, 'create']);
-Route::delete('deleteMotor/{id}', [MotorController::class, 'delete']);
-Route::get('getMotor', [MotorController::class, 'get']);
+// Group route yang memerlukan autentikasi dengan middleware jwt.auth
+Route::group(['middleware' => 'jwt.auth'], function () {
+    // Tambahkan routes yang memerlukan autentikasi di sini
+    // Contoh:
+    Route::post('logout', [AuthController::class, 'logout']);
 
-Route::get('stok', [PenjualanController::class, 'stok']);
-Route::post('penjualan', [PenjualanController::class, 'create']);
+    Route::get('stok', [PenjualanController::class, 'stok']);
+    Route::post('penjualan', [PenjualanController::class, 'create']);
+    Route::get('penjualan', [PenjualanController::class, 'get']);
+    Route::get('penjualan/{kendaraan}', [PenjualanController::class, 'getBy']);
+
+    Route::post('createMobil', [MobilController::class, 'create']);
+    Route::delete('deleteMobil/{id}', [MobilController::class, 'delete']);
+    Route::get('getMobil', [MobilController::class, 'get']);
+
+    Route::post('createMotor', [MotorController::class, 'create']);
+    Route::delete('deleteMotor/{id}', [MotorController::class, 'delete']);
+    Route::get('getMotor', [MotorController::class, 'get']);
+
+    Route::get('getKendaraan/{id}', [KendaraanController::class, 'byId']);
+    Route::get('getKendaraan', [KendaraanController::class, 'get']);
+});
